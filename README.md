54f066d7589017c9ab621b0cd298ccff76c38aa9
Daniel Canales <danielcanalestaylor@hotmail.com>
2026-09-14 14:10:40 -0500

    feat/insertion-seed
    Esta refactorizacion hace una sola insercion con .insertMany, usando el
    forEach para crear un array de pokemones en pokemonToInsert

--- README.md
+++ README.md
@@ -18,6 +18,10 @@ npm i -g @nestjs/cli
 ```
 docker compose up -d
 ```
+5. Recostruir Base de datos con la semilla
+```
+localhost:3000/api/v2/seed
+```
 
 ##Stack usado
 * MongoDB
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -14,18 +14,27 @@ export class SeedService {
       private readonly pokemonModel : Model<Pokemon>
     ){}
 
+  
+
   async executeSeed(){
+    await this.pokemonModel.deleteMany({}) //delete* from pokemons (asegura que no haya duplicidad)
+
     //Peticion a PokeApi 
-    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10')
+    const {data} = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=151');
+
+    const pokemonToInsert: { name: string, no: number} [] = []
 
     data.results.forEach(async({name, url})=>{
       const segments = url.split('/')
       const no = +segments[segments.length -2]
 //Esta insercion de datos viene del modulo pokemon, y se hizo una exportacion del MoongooseModule 
-//y una importacion del PokemonModule en SeedModule para que se peuda usar aqui y crear 1 pokemon por cada
+//y una importacion del PokemonModule en SeedModule para que se pueda usar aqui y crear 1 pokemon por cada
 //respuesta de la pokeApi
-      const pokemon = await this.pokemonModel.create({name, no})
-    })
+      pokemonToInsert.push({name, no})
+    });
+//Insercion multiple con insertMany
+  await this.pokemonModel.insertMany(pokemonToInsert)
+
     return 'Seed Executed'
   }
 
